99e1deacc7fbf52f587472d30f2ed43b9fee1491
Erik Waldeck <137786559+ErikW92@users.noreply.github.com>
2025-06-10 17:38:42 +0200

    Update alt-from-filename.php
    Fixed an initial Bug, that throw an error while installing.

--- alt-from-filename.php
+++ alt-from-filename.php
@@ -1,8 +1,9 @@
+<?php
 /*
 Plugin Name: Auto ALT from Filename
 Description: Automatically adds an ALT from the file name to all <img> elements without an ALT attribute.
 Version: 1.0
-Author: 14Agency GmbH / https://14agency.de - Erik Waldeck
+Author: https://14agency.de
 */
 
 add_action('wp_footer', function () {
@@ -15,12 +16,12 @@ add_action('wp_footer', function () {
       const src = img.getAttribute('src');
       if (!src) return;
 
-      const filename = src.split('/').pop();              // e.g. “example-image_name-123.png”
-      const basename = filename.split('.')[0];            // “example-image_name-123”
+      const filename = src.split('/').pop();     
+      const basename = filename.split('.')[0];  
 
       const readable = basename
-        .replace(/[-_]/g, ' ')                             // Convert separators to spaces
-        .replace(/\b\w/g, char => char.toUpperCase());   // Capitalize each word
+        .replace(/[-_]/g, ' ')                    
+        .replace(/\b\w/g, char => char.toUpperCase());
 
       img.setAttribute('alt', readable);
     });
